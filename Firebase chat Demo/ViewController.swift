f97a63fd1b4a3394849af0dedad9d9f4bfe4b471
Mohammed Alsudani <alsudanimohammed@icloud.com>
2023-07-17 10:39:42 +0300

    Initial Commit

---
+++ Firebase chat Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase chat Demo
+//
+//  Created by MohammedAlsudai on 17.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
